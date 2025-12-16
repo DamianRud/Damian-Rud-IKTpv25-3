a229ec6e5886242aa677cd5e50ef358a9d0977ea
opilane <opilane@HP-CZC2349HTF>
2025-12-16 10:01:11 +0200

    menü tehtud üks def funktsion

---
+++ Damian Rud IKTpv25-3/Damian_Rud_IKTpv25_3.py
@@ -0,0 +1,19 @@
+
+palgad=[1000,1200,2500,800,750,1200]
+inimesed=["A","B","C","D","E","F"]
+
+
+    
+while True:
+    print("palgad",palgad)
+    print("inimesed",inimesed)
+    print("Menüü")
+    print("1.Lisa andmed")
+    print("2.Kustuta andmed")
+    print("3.suurim_palk")
+    print("4.vaikseim_palk")
+    print("5.jarjesta_palgad")
+    print("6.vordsed_palgad")
+    print("7.palgaotsing")
+    print("8.Väljumine")
+    
---
+++ Damian Rud IKTpv25-3/module.py
@@ -0,0 +1,13 @@
+from random import*
+from string import*
+
+
+def lisa_andmed():
+    inimesed_number=int(input("sisesta: kui palju inimesi lisada "))
+    for inimese in range(3):
+        nimi=input("sisesta nimi töötaja ")
+        palk=int(input("sisesta palk töötja ja nimi",palk,"ja",nimi))
+        inimesed_number.append(nimi)
+        palk.append(palk)
+
+        
